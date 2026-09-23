ab121861bf51b658e5d254c51f0f73b349cb8499
Ly1922 <ly1922@users.noreply.github.com>
2026-09-23 23:42:14 +0800

    fix(bat): update correct tailscale ip in start banner

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 echo ====================================================
 echo  Tencent Hunyuan 3.5 To API 正在启动...
 echo  本地访问: http://127.0.0.1:7860
-echo  Tailscale访问: http://100.68.95.0:7860
+echo  Tailscale访问: http://100.97.120.94:7860
 echo  API 文档: http://127.0.0.1:7860/docs
 echo ====================================================
 python -m app.main
